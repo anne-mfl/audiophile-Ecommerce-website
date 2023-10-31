fc023da079aebb2d7f0b287dc44932fdefbd20dd
Anne Lee <annelee@annes-macbook-air.home>
2023-10-31 13:00:51 +0100

    update

--- src/store/cartSlice.tsx
+++ src/store/cartSlice.tsx
@@ -11,13 +11,13 @@ const initialCartState: {
   totalPrice: 0
 }
 
-let localStorageCart = JSON.parse(localStorage.getItem('cart') || '{}')
-// console.log(localStorageCart)
+let localStorageCart = JSON.parse(localStorage.getItem('cart') || '{"items": [], "totalQuantity": 0, "totalPrice": 0}')
+console.log(localStorageCart)
 
 const cartSlice = createSlice({
   name: 'cart',
-  // initialState: !localStorageCart ? initialCartState : localStorageCart,
-  initialState: initialCartState,
+  initialState: !localStorageCart ? initialCartState : localStorageCart,
+  // initialState: initialCartState,
   reducers: {
     addItemToCart: (state, action) => {
       const existingItem = state.items.find((item: CartItemType) => item.id === action.payload.id)
